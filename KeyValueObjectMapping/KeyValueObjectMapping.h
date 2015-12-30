ec4442d2f1e8384832a3188bcf9fb52956a0bc1d
Felipe Augusto <felipowsky@gmail.com>
2015-12-30 12:35:04 -0200

    added keyvalueobjectmapping.h

---
+++ KeyValueObjectMapping/KeyValueObjectMapping.h
@@ -0,0 +1,29 @@
+//
+//  KeyValueObjectMapping.h
+//  KeyValueObjectMapping
+//
+//  Created by felipowsky on 12/30/15.
+//  Copyright © 2015 None. All rights reserved.
+//
+
+#import "DCKeyValueObjectMapping.h"
+#import "NSObject+DCKeyValueObjectMapping.h"
+#import "DCPropertyFinder.h"
+#import "DCAttributeSetter.h"
+#import "DCReferenceKeyParser.h"
+#import "DCDictionaryRearranger.h"
+#import "DCCustomInitialize.h"
+#import "DCCustomParser.h"
+#import "DCParserConfiguration.h"
+#import "DCNSArrayConverter.h"
+#import "DCNSSetConverter.h"
+#import "DCNSDateConverter.h"
+#import "DCNSURLConverter.h"
+#import "DCSimpleConverter.h"
+#import "DCValueConverter.h"
+#import "DCGenericConverter.h"
+#import "DCDynamicAttribute.h"
+#import "DCObjectMapping.h"
+#import "DCArrayMapping.h"
+#import "DCPropertyAggregator.h"
+#import "DCMapping.h"
